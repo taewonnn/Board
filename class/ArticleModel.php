b1f18e800788a06d5fd114dbbbc175412d50cd70
TaeWon Park <thisis_tw94@naver.com>
2023-10-06 10:00:00 +0900

    articleModel

--- class/ArticleModel.php
+++ class/ArticleModel.php
@@ -4,16 +4,21 @@ require_once(__DIR__ . '/DatabaseClient.php');
 
 class ArticleModel extends DatabaseClient{
 
+    // 부모 클래스의 생성자를 호출
     public function __construct() {
-        parent::__construct(); // 부모 클래스의 생성자를 호출
+        parent::__construct(); 
     }
 
+    /**
+     * get Article list
+     * @retrun array
+     */
     public function getArticle() {
         // 쿼리
         $sql = "SELECT articles.*, users.name AS author_name
-          FROM articles
-          JOIN users ON articles.users_id = users.id
-          ORDER BY articles.created_at DESC
+            FROM articles
+            JOIN users ON articles.users_id = users.id
+            ORDER BY articles.created_at DESC
         ";
 
         // 쿼리 실행
