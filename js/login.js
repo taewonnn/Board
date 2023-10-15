b47837fe041a6075ddc7cec829b2c020bebbcefa
TaeWon Park <thisis_tw94@naver.com>
2023-10-15 20:49:40 +0900

    login

--- js/login.js
+++ js/login.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 요소 설정
-    const loginForm = document.querySelector('form')
+    const loginFrom = document.querySelector('form')
     const errorMessage = document.getElementById('errorMessage')
 
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault() // 기본 제출 동작 방지
+    // 쿠키 설정 함수
+    const setCookie = (name, value, minutes) => {
+        const expirationDate = new Date(
+            new Date().getTime() + minutes * 60 * 1000
+        )
+        document.cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`
+    }
 
-        const email = document.querySelector('input[name="email"]').value
-        const password = document.querySelector('input[name="password"]').value
-
-        // 로그인 데이터를 서버로 전송
+    const login = (email, password) => {
         fetch('/include/login.php', {
             method: 'POST',
             headers: {
@@ -17,96 +19,31 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: `email=${email}&password=${password}`,
         })
-            .then((response) => {
-                console.log(response)
-                if (!response.ok) {
-                    // 서버에서 응답이 ok가 아니면 에러 메시지를 반환합니다.
-                    return response.json().then((err) => {
-                        throw err
-                    })
-                }
-                // 정상 응답 반환
-                return response.json()
+            .then((res) => {
+                return res.json()
             })
             .then((data) => {
-                if (data.error) {
-                    // 에러 메시지 표시
-                    errorMessage.innerText = data.error
-                } else {
-                    // 로그인이 성공한 경우 쿠키로 사용자 정보 저장 (60분 동안 유효)
-                    const expirationDate = new Date(
-                        new Date().getTime() + 60 * 60 * 1000
-                    )
-                    document.cookie = `user_id=${
-                        data.id
-                    }; expires=${expirationDate.toUTCString()}; path=/`
-                    document.cookie = `user_name=${
-                        data.name
-                    }; expires=${expirationDate.toUTCString()}; path=/`
-
-                    // 로그인이 성공한 경우 리다이렉트 또는 원하는 작업 수행
+                console.log(data)
+                if (data.status === 200) {
+                    // 로그인 성공 시 - 쿠키 셋팅
+                    setCookie('user_id', data.data.id, 60)
+                    setCookie('user_name', data.data.name, 60)
+                    alert('로그인 성공')
                     window.location.href = '/index.html'
+                } else {
+                    errorMessage.innerText = data.message
                 }
             })
             .catch((error) => {
-                console.error(error)
-                errorMessage.innerText = error.error || '다시 시도해주세요!'
+                console.error('Error: ', error)
+                alert('다시 시도해주세요!')
             })
-    })
-})
-document.addEventListener('DOMContentLoaded', () => {
-    // 요소 설정
-    const loginForm = document.querySelector('form')
-    const errorMessage = document.getElementById('errorMessage')
+    }
 
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault() // 기본 제출 동작 방지
-
+    loginFrom.addEventListener('submit', (e) => {
+        e.preventDefault()
         const email = document.querySelector('input[name="email"]').value
-        const password = document.querySelector('input[name="password"]').value
-
-        // 로그인 데이터를 서버로 전송
-        fetch('/include/login.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `email=${email}&password=${password}`,
-        })
-            .then((response) => {
-                console.log(response)
-                if (!response.ok) {
-                    // 서버에서 응답이 ok가 아니면 에러 메시지를 반환합니다.
-                    return response.json().then((err) => {
-                        throw err
-                    })
-                }
-                // 정상 응답 반환
-                return response.json()
-            })
-            .then((data) => {
-                if (data.error) {
-                    // 에러 메시지 표시
-                    errorMessage.innerText = data.error
-                } else {
-                    // 로그인이 성공한 경우 쿠키로 사용자 정보 저장 (60분 동안 유효)
-                    const expirationDate = new Date(
-                        new Date().getTime() + 60 * 60 * 1000
-                    )
-                    document.cookie = `user_id=${
-                        data.id
-                    }; expires=${expirationDate.toUTCString()}; path=/`
-                    document.cookie = `user_name=${
-                        data.name
-                    }; expires=${expirationDate.toUTCString()}; path=/`
-
-                    // 로그인이 성공한 경우 리다이렉트 또는 원하는 작업 수행
-                    window.location.href = '/index.html'
-                }
-            })
-            .catch((error) => {
-                console.error(error)
-                errorMessage.innerText = error.error || '다시 시도해주세요!'
-            })
+        const password = document.querySelector('input[name="password').value
+        login(email, password)
     })
 })
