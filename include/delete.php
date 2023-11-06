cab2d1a764d7bb08be7c1515ab3b1d82e30219ad
TaeWon Park <thisis_tw94@naver.com>
2023-11-06 21:28:13 +0900

    delete

--- include/delete.php
+++ include/delete.php
@@ -1,4 +1,5 @@
 <?php
+
     require_once(__DIR__ . '/../class/ArticleModel.php');
     require_once(__DIR__ . '/../class/APIResponse.php');
 
--- js/delete.js
+++ js/delete.js
@@ -6,8 +6,8 @@ async function fetchDeleteData(url, data) {
         headers: {
             'Content-Type': 'application/json',
         },
-    })
-    return response
+    });
+    return response;
 }
 
 // 삭제 버튼 클릭 이벤트
@@ -15,35 +15,35 @@ document.getElementById('deleteButton').addEventListener('click', async () => {
     try {
         if (confirm('게시글을 삭제하시겠습니까?')) {
             // URL에서 게시글 id 파싱
-            const urlParams = new URLSearchParams(window.location.search)
-            const articleId = urlParams.get('id')
+            const urlParams = new URLSearchParams(window.location.search);
+            const articleId = urlParams.get('id');
 
             // 사용자 id
-            const userId = getCookie('user_id')
+            const userId = getCookie('user_id');
 
             // JSON 데이터 준비
             const data = {
                 article_id: articleId,
                 user_id: userId,
-            }
+            };
 
             // fetch
-            const res = await fetchDeleteData('/include/delete.php', data)
-            console.log('res 확인', res)
+            const res = await fetchDeleteData('/include/delete.php', data);
+            console.log('res 확인', res);
 
             if (res.status !== 200) {
                 // 에러 발생 시
-                const result = await res.json()
-                throw new Error(result.message)
+                const result = await res.json();
+                throw new Error(result.message);
             } else {
                 // 성공 시
-                const data = await res.json()
-                alert(data.message)
-                window.location.href = '/pages/list.html'
+                const data = await res.json();
+                alert(data.message);
+                window.location.href = '/pages/list.html';
             }
-        }
+        } else return;
     } catch (error) {
-        console.log(error)
-        alert(error.message)
+        console.log(error);
+        alert(error.message);
     }
-})
+});
