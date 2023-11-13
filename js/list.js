3442c1ae2dddc44841fe7a1924747b465f6b9352
TaeWon Park <thisis_tw94@naver.com>
2023-11-13 10:00:00 +0900

    common

--- js/list.js
+++ js/list.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', async () => {
     try {
-        const res = await fetch('../include/list.php')
-        const { data } = await res.json()
+        const res = await fetch('../include/list.php');
+        const { data } = await res.json();
 
-        const container = document.getElementById('articleContainer')
+        const container = document.getElementById('articleContainer');
 
         data.forEach((article) => {
-            console.log(article)
+            console.log(article);
 
             // Card 생성
-            const card = document.createElement('div')
-            card.className = 'card'
-            card.style.width = '18rem'
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style.width = '18rem';
 
             // Card Image 생성
             // const img = document.createElement("img");
@@ -20,35 +20,35 @@ window.addEventListener('DOMContentLoaded', async () => {
             // card.appendChild(img);
 
             // Card Body 생성
-            const cardBody = document.createElement('div')
-            cardBody.className = 'card-body'
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
 
             // Card Title 생성
-            const cardTitle = document.createElement('h5')
-            cardTitle.className = 'card-title'
-            cardTitle.innerHTML = article.title
-            cardBody.appendChild(cardTitle)
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.innerHTML = article.title;
+            cardBody.appendChild(cardTitle);
 
             // Card Text 생성
-            const cardText = document.createElement('p')
-            cardText.className = 'card-text'
-            cardText.innerHTML = truncateTextByWords(article.content, 3)
-            cardBody.appendChild(cardText)
+            const cardText = document.createElement('p');
+            cardText.className = 'card-text';
+            cardText.innerHTML = truncateTextByWords(article.content, 3);
+            cardBody.appendChild(cardText);
 
             // Button 추가
-            const button = document.createElement('a')
-            button.href = `detail.html?id=${article.id}`
-            button.className = 'btn btn-primary'
-            button.innerHTML = '더보기'
-            cardBody.appendChild(button)
+            const button = document.createElement('a');
+            button.href = `detail.html?id=${article.id}`;
+            button.className = 'btn btn-primary';
+            button.innerHTML = '더보기';
+            cardBody.appendChild(button);
 
             // cardBody를 card에 추가
-            card.appendChild(cardBody)
+            card.appendChild(cardBody);
 
             // card를 container에 추가
-            container.appendChild(card)
-        })
+            container.appendChild(card);
+        });
     } catch (error) {
-        console.log('fail', error)
+        console.log('fail', error);
     }
-})
+});
