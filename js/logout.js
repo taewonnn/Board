1304a7fc5da2e1b26b68dc3b9ceec7674ae6c25e
TaeWon Park <thisis_tw94@naver.com>
2023-11-19 22:39:05 +0900

    logout

--- js/logout.js
+++ js/logout.js
@@ -1,16 +1,16 @@
 /* 사용X */
 document.addEventListener('DOMContentLoaded', () => {
-    const logoutButton = document.getElementById('logoutButton')
+    const logoutButton = document.getElementById('logoutButton');
 
-    console.log('확인', logoutButton)
+    console.log('확인', logoutButton);
     if (logoutButton) {
         logoutButton.addEventListener('click', () => {
             // 쿠키 삭제
-            document.cookie = 'user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
-            document.cookie = 'user_name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+            document.cookie = 'user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+            document.cookie = 'user_name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
             // 홈 페이지로 리디렉션
-            window.location.href = '/index.html'
-        })
+            window.location.href = '/index.html';
+        });
     }
-})
+});
