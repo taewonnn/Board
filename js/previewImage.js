035dc56124f4196604c2d5edeb1a906df21b02a8
TaeWon Park <thisis_tw94@naver.com>
2023-11-22 21:54:32 +0900

    preview

--- js/previewImage.js
+++ js/previewImage.js
@@ -1,23 +1,23 @@
 // 이미지 미리보기
 window.addEventListener('load', () => {
-    const imgInput = document.getElementById('image')
-    const imgPreview = document.getElementById('imagePreview')
+    const imgInput = document.getElementById('image');
+    const imgPreview = document.getElementById('imagePreview');
 
     imgInput.addEventListener('change', (e) => {
-        const file = e.target.files[0]
-        const reader = new FileReader()
+        const file = e.target.files[0];
+        const reader = new FileReader();
 
         reader.onload = function (event) {
-            console.log(event.target)
-            imgPreview.src = event.target.result
-            imgPreview.style.display = 'block'
-        }
+            console.log(event.target);
+            imgPreview.src = event.target.result;
+            imgPreview.style.display = 'block';
+        };
 
         if (file) {
-            reader.readAsDataURL(file)
+            reader.readAsDataURL(file);
         } else {
-            imgPreview.src = ''
-            imgPreview.style.display = 'none'
+            imgPreview.src = '';
+            imgPreview.style.display = 'none';
         }
-    })
-})
+    });
+});
